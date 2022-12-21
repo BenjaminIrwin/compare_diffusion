60e636d0453a64501b31884e8e7b80b525893af5
BenjaminIrwin <benjamintdirwin@gmail.com>
2022-12-21 12:41:23 +0000

    Changed font path

--- pdf_gen.py
+++ pdf_gen.py
@@ -117,7 +117,7 @@ def create_text_image(text='final font size', width=512, height=512, x_justify=0
 
     draw = ImageDraw.Draw(img)
 
-    font = ImageFont.truetype("/System/Library/Fonts/Monaco.ttf", fontsize)
+    font = ImageFont.truetype("Monaco.ttf", fontsize)
 
     print(text, fontsize)
 
