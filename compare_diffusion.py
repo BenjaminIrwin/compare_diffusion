e6a8bd30c5a55d89f4aba8995d6277406354efb4
BenjaminIrwin <benjamintdirwin@gmail.com>
2022-12-21 12:39:32 +0000

    Comment out try catch

--- compare_diffusion.py
+++ compare_diffusion.py
@@ -230,31 +230,31 @@ def generate_images():
                                          f'neg_pmt_{negative_prompt}/cfg_{cfg_scale}/den_{denoising_strength}/seed_{seed}'
                                 create_folder(folder)
                                 for idx, image in enumerate(images):
-                                    try:
-                                        pil_image = Image.open(image)
-                                        image_name = image.split('/')[-1]
-                                        if type == 'inpaint':
-                                            # Call inpaint
-                                            pil_mask = Image.open(masks[idx])
-                                            output = model(prompt=prompt, image=pil_image, mask_image=pil_mask,
-                                                           guidance_scale=cfg_scale,
-                                                           generator=generator, strength=denoising_strength, height=height,
-                                                           width=width).images[0]
-                                            output.save(folder + '/' + str(image_name))
-                                            output_counter += 1
-                                            terminal_progress_bar(output_counter, num_images_to_generate)
-                                        elif type == 'img2img':
-                                            # Call img2img
-                                            output = model(prompt=prompt, image=pil_image, guidance_scale=cfg_scale,
-                                                           generator=generator, strength=denoising_strength, height=height,
-                                                           width=width).images[0]
-                                            output.save(folder + '/' + str(image_name))
-                                            output_counter += 1
-                                            terminal_progress_bar(output_counter, num_images_to_generate)
-                                    except Exception as e:
-                                        print('Error generating image with params: ' + str(prompt) + ' ' + str(
-                                            negative_prompt) + ' ' + str(cfg_scale) + ' ' + str(denoising_strength))
-                                        print(e)
+                                    # try:
+                                    pil_image = Image.open(image)
+                                    image_name = image.split('/')[-1]
+                                    if type == 'inpaint':
+                                        # Call inpaint
+                                        pil_mask = Image.open(masks[idx])
+                                        output = model(prompt=prompt, image=pil_image, mask_image=pil_mask,
+                                                       guidance_scale=cfg_scale,
+                                                       generator=generator, strength=denoising_strength, height=height,
+                                                       width=width).images[0]
+                                        output.save(folder + '/' + str(image_name))
+                                        output_counter += 1
+                                        terminal_progress_bar(output_counter, num_images_to_generate)
+                                    elif type == 'img2img':
+                                        # Call img2img
+                                        output = model(prompt=prompt, image=pil_image, guidance_scale=cfg_scale,
+                                                       generator=generator, strength=denoising_strength, height=height,
+                                                       width=width).images[0]
+                                        output.save(folder + '/' + str(image_name))
+                                        output_counter += 1
+                                        terminal_progress_bar(output_counter, num_images_to_generate)
+                                    # except Exception as e:
+                                    #     print('Error generating image with params: ' + str(prompt) + ' ' + str(
+                                    #         negative_prompt) + ' ' + str(cfg_scale) + ' ' + str(denoising_strength))
+                                    #     print(e)
 
 
 if __name__ == "__main__":
