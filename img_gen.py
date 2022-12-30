29b4d4193b1133d8551f0a5c6d82c686c613394c
BenjaminIrwin <benjamintdirwin@gmail.com>
2022-12-30 16:00:57 +0000

    Changed image resizing

--- img_gen.py
+++ img_gen.py
@@ -17,13 +17,7 @@ def apply_overlay(image, paste_loc, overlay):
     if paste_loc is not None:
         x, y, width, height = paste_loc
         base_image = Image.new('RGBA', (overlay.width, overlay.height))
-        ratio = width / height
-        src_ratio = image.width / image.height
-        src_w = width if ratio > src_ratio else image.width * height // image.height
-        src_h = height if ratio <= src_ratio else image.height * width // image.width
-        resized = resize(image, src_w, src_h)
-        res = Image.new("RGB", (width, height))
-        res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
+        image = resize_image(1, image, width, height)
         base_image.paste(image, (x, y))
         image = base_image
 
@@ -34,9 +28,59 @@ def apply_overlay(image, paste_loc, overlay):
     return image
 
 
-def resize(im, w, h):
-    return im.resize((w, h), resample=LANCZOS)
+def resize(im, width, height):
+    ratio = width / height
+    src_ratio = im.width / im.height
 
+    src_w = width if ratio > src_ratio else im.width * height // im.height
+    src_h = height if ratio <= src_ratio else im.height * width // im.width
+
+    resized = im.resize((width, height), resample=LANCZOS)
+    res = Image.new("RGB", (width, height))
+    res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
+
+    return res
+
+
+def resize_image(resize_mode, im, width, height):
+    def resize(im, w, h):
+        return im.resize((w, h), resample=LANCZOS)
+
+    if resize_mode == 0:
+        res = resize(im, width, height)
+
+    elif resize_mode == 1:
+        ratio = width / height
+        src_ratio = im.width / im.height
+
+        src_w = width if ratio > src_ratio else im.width * height // im.height
+        src_h = height if ratio <= src_ratio else im.height * width // im.width
+
+        resized = resize(im, src_w, src_h)
+        res = Image.new("RGB", (width, height))
+        res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
+
+    else:
+        ratio = width / height
+        src_ratio = im.width / im.height
+
+        src_w = width if ratio < src_ratio else im.width * height // im.height
+        src_h = height if ratio >= src_ratio else im.height * width // im.width
+
+        resized = resize(im, src_w, src_h)
+        res = Image.new("RGB", (width, height))
+        res.paste(resized, box=(width // 2 - src_w // 2, height // 2 - src_h // 2))
+
+        if ratio < src_ratio:
+            fill_height = height // 2 - src_h // 2
+            res.paste(resized.resize((width, fill_height), box=(0, 0, width, 0)), box=(0, 0))
+            res.paste(resized.resize((width, fill_height), box=(0, resized.height, width, resized.height)), box=(0, fill_height + src_h))
+        elif ratio > src_ratio:
+            fill_width = width // 2 - src_w // 2
+            res.paste(resized.resize((fill_width, height), box=(0, 0, 0, height)), box=(0, 0))
+            res.paste(resized.resize((fill_width, height), box=(resized.width, 0, resized.width, height)), box=(fill_width + src_w, 0))
+
+    return res
 
 def generate_images(args, images, masks):
     hf_token = args['hf_token']
@@ -106,7 +150,7 @@ def generate_images(args, images, masks):
                                             x1, y1, x2, y2 = crop_region
 
                                             mask = mask.crop(crop_region)
-                                            pil_mask = images.resize_image(2, mask, width, height)
+                                            pil_mask = images.resize_image(2, mask, pil_image.width, pil_image.height)
                                             paste_to = (x1, y1, x2 - x1, y2 - y1)
                                         else:
                                             paste_to = None
